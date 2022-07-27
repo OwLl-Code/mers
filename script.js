fd876b4de3fdc86813151a860efe7250ad4082cd
OwLl-Code <102817853+OwLl-Code@users.noreply.github.com>
2022-07-27 19:34:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var modal = document.getElementById('modal-window');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+
+
