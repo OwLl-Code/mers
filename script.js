1e0849d295f564ca35f63f7e85b85392ae5f18a6
OwLl-Code <102817853+OwLl-Code@users.noreply.github.com>
2022-08-01 19:28:02 +0300

    add: modal and changing the background

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-var modal = document.getElementById('modal-window');
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+///смена цвета
+var colorArray = ["#1b1d1e", "#2d3037", "#373a53","#3a4280"];
+var i = 0;
 
-btn.onclick = function() {
-    modal.style.display = "block";
+function changeColor(){
+    document.body.style.background = colorArray[i];
+    i++;
+    if( i >= colorArray.length) {
+        i = 0;
+    }
 }
 
-span.onclick = function() {
-    modal.style.display = "none";
+////modal
+const openModal = (triggerSelector, modalDataSelector) => {
+    const trigger = document.querySelector(triggerSelector)
+    const modal = document.querySelector(modalDataSelector)
+    if (!trigger || !modal) return
+    trigger.addEventListener('click', e => {
+        e.preventDefault()
+        modal.classList.add('modal_active')
+    })
 }
+openModal('.buttons__button_one', '.modal[data-modal="one"]')
+openModal('.buttons__button_two', '.modal[data-modal="two"]')
 
-
-
-
+const closeModal = () => {
+    const modals = document.querySelectorAll('.modal')
+    if (!modals) return
+    modals.forEach(el => {
+        el.addEventListener('click', e => {
+            if (e.target.closest('.modal__close')) {
+                el.classList.remove('modal_active')
+            }
+            if (!e.target.closest('.modal__body')) {
+                el.classList.remove('modal_active')
+            }
+        })
+    })
+}
+closeModal()
